8f69bd62731c7969a68ab311960d9bec2959dcea
吕途 <buzhidaolvtu@163.com>
2018-10-20 20:51:18 +0800

    修改

--- src/main/java/net/opensource/freedom/util/ZipUtils.java
+++ src/main/java/net/opensource/freedom/util/ZipUtils.java
@@ -6,14 +6,18 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Enumeration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-public class ZipUtils {
+public abstract class ZipUtils {
 
-    public static void unzip(File srcFile, File targetDir) {
-        try (ZipFile zipFile = new ZipFile(srcFile)) {
+    public static void unzip(File srcZipFile, File targetDir) {
+        try (ZipFile zipFile = new ZipFile(srcZipFile)) {
             Enumeration<? extends ZipEntry> entries = zipFile.entries();
             while (entries.hasMoreElements()) {
                 ZipEntry entry = entries.nextElement();
@@ -33,4 +37,32 @@ public class ZipUtils {
             throw new RuntimeException(ex);
         }
     }
+
+    public static File unzip(File srcZipFile) {
+        try {
+            Path tempDirectory = Files.createTempDirectory("util4j-unzip");
+            unzip(srcZipFile, tempDirectory.toFile());
+            return tempDirectory.toFile();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private final static Pattern OuterJarArchivePattern = Pattern.compile("file:(.*\\.jar)!(.*\\.jar)");
+
+    public static String proceedIfWithOuterJar(String path) {
+        Matcher matcher = OuterJarArchivePattern.matcher(path);
+        if (matcher.find()) {
+            String outerJarPath = matcher.group(1);
+            String nestedJarPath = matcher.group(2);
+            return unzip(new File(outerJarPath)) + nestedJarPath;
+        }
+        return path;
+    }
+
+    public static void main(String[] args) {
+        String file = "file:/Users/lvtu/workspace/sandbox-boot-agent/target/sandbox-boot-agent.jar!/sandbox/lib/sandbox-agent.jar!/";
+        System.out.println(proceedIfWithOuterJar(file));
+    }
+
 }
--- src/test/java/net/opensource/freedom/util/ZipUtilsTest.java
+++ src/test/java/net/opensource/freedom/util/ZipUtilsTest.java
@@ -2,10 +2,17 @@ package net.opensource.freedom.util;
 
 
 import org.junit.Test;
+import org.springframework.boot.loader.archive.Archive;
+import org.springframework.boot.loader.archive.ExplodedArchive;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 
 public class ZipUtilsTest {
 
@@ -17,5 +24,41 @@ public class ZipUtilsTest {
         ZipUtils.unzip(file, unzip.toFile());
     }
 
+    public static String process(URL url) {
+        return url.toString().replace("!", "").replace("jar:file:", "");
+    }
+
+    public static void todoMethod() {
+        try {
+            File f = new File("/Users/lvtu/workspace/util4j/target/boot.jar");
+            File unzip = ZipUtils.unzip(f);
+//            Archive explodedArchive = new JarFileArchive(f);
+            Archive explodedArchive = new ExplodedArchive(unzip);
+            List<Archive> nestedArchives = explodedArchive.getNestedArchives(entry -> entry.getName().endsWith(".jar"));
+            List<URL> urlList = nestedArchives.stream().map(nestedArchive -> {
+                try {
+                    return nestedArchive.getUrl();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }).collect(Collectors.toList());
+            urlList.forEach(url -> {
+                System.out.println(url);
+            });
+            List<JarFile> jarFiles = urlList.stream().map(url -> {
+                try {
+                    return new JarFile(process(url));
+//                    return new JarFile();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }).collect(Collectors.toList());
+            System.out.println(jarFiles);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
 }
